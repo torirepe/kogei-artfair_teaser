1f99ce635e2937f4f237c2a247835ad18c7e8430
torirepe <t.motoe@amd.tokyo>
2017-04-24 19:57:04 +0900

    追記

--- source/common/js/script.js
+++ source/common/js/script.js
@@ -1,16 +1,48 @@
-$(window).load(function(){
-  if($("#heroSlider")) {
-    heroSlider()
-  }
-});
-
-function heroSlider(){
-  var target = $("#heroSlider");
-  target.bxSlider({
-    auto: true,
-    speed: 600,
-    pager: true,
-    controls: true,
-    infiniteLoop: true,
+$(function(){
+  //extensionBox
+  $(document).on('click','#extensionBox a', function(){
+    $('#extensionBox').toggleClass("active");
   });
-}
+  
+  //href #
+  $(document).on('click', 'a[href^=#]', function () {
+    var speed = 360;
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
+  
+  $('#language').click(function(){
+    var next;
+    var body = $('body');
+    var wrapper = $('#wrapper');
+    if(body.hasClass('jp')) {
+      next = 'en';
+    } else if(body.hasClass('en')) {
+      next = 'jp';
+    }
+    $.ajax(next + '.html', {
+      timeout : 1000,
+      datatype:'html'
+    }).then(function(data){
+      var outHtml = $($.parseHTML(data));
+      var shapeHtml = $(outHtml.filter('#' + next)[0].innerHTML);
+      wrapper.animate({
+        opacity: 0
+      }, 360, 'swing', function() {
+        body.removeClass();
+        body.addClass(next);
+        $('#wrapper').empty().append(shapeHtml);
+        shapeHtml.ready(function() {
+          wrapper.animate({ opacity: 1 }, 600, 'swing');
+        });
+      });
+    },function(jqXHR, textStatus) {
+      alert(textStatus);
+    })
+  });
+});
